26bc3dc08df9278d70c9041bbe96b93840869819
paulhji <thisispaulji@gmail.com>
2024-01-25 18:56:45 -0800

    updating database design file and sql files

---
+++ query.sql
@@ -0,0 +1,10 @@
+-- Database querying
+
+-- Collision database
+SELECT * FROM collision;
+
+-- People_affected database
+SELECT * FROM people_affected;
+
+-- Vehicle_info database
+SELECT * FROM vehicle_info;
